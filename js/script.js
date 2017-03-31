4945e3a76b43aac3edd61b34a258aea043dcb81f
Turetski <turetski212@gmail.com>
2017-03-31 13:22:06 +0700

    First steps with AngularJS

---
+++ js/script.js
@@ -0,0 +1,53 @@
+(function(){ 
+  var AppStore = angular.module('AppStore',[]);
+  function parseDate(myDate){
+      return "".concat(myDate.getDate(), " ", parseMonth(myDate.getMonth()), " ", myDate.getFullYear() );
+  };
+  function parseMonth(m){
+    var months ={
+      '0': "января", '1': "февраля",
+      '2': "марта", '3': "апреля",
+      '4': "мая", '5': "июня",
+      '6': "июля", '7': "августа",
+      '8': "сентября", '9': "октября",
+      '10': "ноября",'11': "декабря"
+    };
+    return  months[m];
+  }
+  var apps = [
+    { name:"Учет сейфовых ячеек",
+      date: new Date(1473379200*1000),
+      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
+      price: "4.99"
+    },
+    { name:"Учет сейфовых ячеек",
+      date: new Date(1473292800*1000),
+      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
+      price: "4.87"
+    },
+    { name:"Учет сейфовых ячеек",
+      date: new Date(1473206400*1000),
+      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
+      price: "4.82"
+    },
+    { name:"Учет сейфовых ячеек",
+      date: new Date(1473120000*1000),
+      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
+      price: "4.75"
+    },
+    { name:"Учет сейфовых ячеек",
+      date: new Date(1473033600*1000),
+      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
+      price: "4.70"
+    }
+  ];
+  AppStore.controller("appTable", function(){
+    this.apps = apps;
+  });
+  AppStore.directive("appItem",function(){
+    return {
+      restrict: "A",
+      templateUrl: "source/appItem.html"
+    };
+  });
+})();
