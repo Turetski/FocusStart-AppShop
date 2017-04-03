8a6c05f20c0bf49c84661512221498252a0e58cb
Turetski <turetski212@gmail.com>
2017-04-03 10:10:44 +0700

    loading data from .json released

--- app/js/script.js
+++ app/js/script.js
@@ -1,48 +1,28 @@
 (function(){ 
   var AppStore = angular.module('AppStore',['Appstore-directives']);
-  function parseDate(myDate){
-      return "".concat(myDate.getDate(), " ", parseMonth(myDate.getMonth()), " ", myDate.getFullYear() );
-  };
-  function parseMonth(m){
-    var months ={
-      '0': "января", '1': "февраля",
-      '2': "марта", '3': "апреля",
-      '4': "мая", '5': "июня",
-      '6': "июля", '7': "августа",
-      '8': "сентября", '9': "октября",
-      '10': "ноября",'11': "декабря"
-    };
-    return  months[m];
-  }
-  var apps = [
-    { name:"Учет сейфовых ячеек",
-      date: new Date(1473379200*1000),
-      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
-      price: "4.99"
-    },
-    { name:"Учет сейфовых ячеек",
-      date: new Date(1473292800*1000),
-      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
-      price: "4.87"
-    },
-    { name:"Учет сейфовых ячеек",
-      date: new Date(1473206400*1000),
-      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
-      price: "4.82"
-    },
-    { name:"Учет сейфовых ячеек",
-      date: new Date(1473120000*1000),
-      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
-      price: "4.75"
-    },
-    { name:"Учет сейфовых ячеек",
-      date: new Date(1473033600*1000),
-      description: "Аналитический учет сейфовых ячеек банка по подразделениям банка.",
-      price: "4.70"
+
+  AppStore.factory('httpq', function($http, $q){
+    return{
+        get: function(){
+            var deffered = $q.defer();
+            $http.get.apply(null, arguments)
+                .then(deffered.resolve)
+                .catch(deffered.resolve);
+                return deffered.promise;
+       }
     }
-  ];
-  AppStore.controller("appTable", function(){
-    this.apps = apps;
+  });
+
+  AppStore.controller("appTable", function(httpq){
+    var store = this;
+    store.apps =[];
+    httpq.get('api/products.json')
+        .then(function(result){
+            store.apps = result.data;
+        })
+        .catch(function(){
+            console.log('error');
+        });
   });
 
   AppStore.controller("panelCtrl", function(){
